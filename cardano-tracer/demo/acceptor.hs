5066b2babeb8bba4f15f110abf2b44c6c01c7728
Mark Tullsen <tullsen@galois.com>
2023-04-12 14:34:40 -0700

    extend 'demo-acceptor' with further functionality:  - multiple servers  - multiple datapoints  - multiple frequencies  - parameterizable EKG frequency

--- cardano-tracer/demo/acceptor.hs
+++ cardano-tracer/demo/acceptor.hs
@@ -1,11 +1,85 @@
 {-# LANGUAGE LambdaCase #-}
 
+import Control.Arrow (first)
+import Control.Monad (unless,when)
+import Data.Fixed (Pico)
 import System.Environment (getArgs)
+import System.Exit (exitFailure)
 
 import Cardano.Tracer.Test.Acceptor
 
+main' :: IO ()
+main' = do
+  as <- getArgs
+  localSock:ir:as' <- if length as >= 3 then
+                        return as
+                      else
+                        usageFail "not enough arguments"
+  ir' <- case ir of 
+           "Initiator" -> return Initiator
+           "Responder" -> return Responder
+           _           -> usageFail ""
+
+  dps' <- processDPs as'
+  
+  launchAcceptorsSimple ir' localSock dps'
+
+
+-- FIXME: time to clone from demo-acceptor.
+
 main :: IO ()
-main = getArgs >>= \case
-  [localSock, "Initiator", dpName] -> launchAcceptorsSimple Initiator localSock dpName
-  [localSock, "Responder", dpName] -> launchAcceptorsSimple Responder localSock dpName
-  _ -> putStrLn "Usage: ./demo-acceptor /path/to/local/sock Initiator|Responder Name.Of.DataPoint"
+main = do
+  as <- getArgs
+  (ekgFreq,as') <- case as of
+                     "-e":ekg:as' -> return (read ekg :: Pico, as')
+                     _            -> return (10/1            , as )
+                     
+  (localSockets,as'') <- parseLocalSockets as'
+  when (null localSockets) $
+    usageFail "no sockets specified"
+  dps' <- processDPs as''
+  launchInitiators ekgFreq localSockets dps'
+
+
+parseLocalSockets :: [String] -> IO ([String],[String])
+parseLocalSockets ("-s":s:xs) = first (s:) <$> parseLocalSockets xs
+parseLocalSockets ["-s"]      = usageFail "-s without path"
+parseLocalSockets xs          = return ([],xs)
+
+
+-- | create groups of DPs for multiple periods
+processDPs :: [String] -> IO [(Double, [String])]
+processDPs as' =
+  do
+  dps <- go (addPeriod dfltPeriod []) as'
+  return
+    $ reverse
+    $ filter (\(_,ns)-> not(null ns)) dps
+  where
+  dfltPeriod = 1.0 :: Double
+  
+  go dps = \case
+    []        -> return dps
+    ["-p"]    -> usageFail "-p without 'period' argument"
+    "-p":a:as -> go (addPeriod (read a :: Double) dps) as   -- FIXME
+    a:as      -> go (addDP a dps)                      as
+
+  addPeriod p xs      = (p,[]):xs
+  
+  addDP d ((p,ds):xs) = (p,ds++[d]):xs
+  addDP _ []          = error "impossible"
+  
+  -- FIXME: input sanitization
+  --  - no datapoints
+  --  - `read a`
+  --  - warn if a DP in multiple sets ?
+  
+usageFail :: String -> IO a
+usageFail s = do
+  unless (null s) $
+    do
+    putStr "Error:\n  "
+    putStrLn s
+  putStrLn "Usage: demo-acceptor [-e ekgfreq] [-s /path/to/local/sock]+ [Name.Of.DataPoint | -p seconds]+ "
+  exitFailure
+
--- cardano-tracer/test/Cardano/Tracer/Test/Acceptor.hs
+++ cardano-tracer/test/Cardano/Tracer/Test/Acceptor.hs
@@ -1,21 +1,30 @@
+{-# LANGUAGE LambdaCase #-}
 {-# LANGUAGE DataKinds #-}
 {-# LANGUAGE OverloadedStrings #-}
 
 module Cardano.Tracer.Test.Acceptor
   ( AcceptorsMode (..)
   , launchAcceptorsSimple
+  , launchInitiators
   ) where
 
 import           Control.Concurrent.STM.TVar (newTVarIO, readTVarIO)
 import           Control.Concurrent.Async.Extra (sequenceConcurrently)
 import           Control.Concurrent.Extra (newLock)
-import           Control.Monad (forever, forM_, void)
+import           Control.Monad (forever, forM_, unless, void)
 import qualified Data.ByteString.Lazy as LBS
+import           Data.Fixed (Pico)
+import           Data.IORef
 import qualified Data.List.NonEmpty as NE
 import qualified Data.Map.Strict as M
 import qualified Data.Text as T
-import           System.Time.Extra (sleep)
+import           Data.Time.Clock (UTCTime(..))
+import           Data.Time.Calendar (Day(..))
+import           System.IO
+import           System.Time.Extra (sleep,Seconds)
 
+import           Data.Aeson (Value(..),decode,encode)
+                  
 import           Cardano.Tracer.Environment
 import           Cardano.Tracer.Acceptors.Run
 import           Cardano.Tracer.Configuration
@@ -31,9 +40,10 @@ data AcceptorsMode = Initiator | Responder
 launchAcceptorsSimple
   :: AcceptorsMode
   -> FilePath
-  -> String
+  -> [(Seconds,[DPName])] -- for each period, a groups of datapoints to query
   -> IO ()
-launchAcceptorsSimple mode localSock dpName = do
+launchAcceptorsSimple mode localSock dpGroups = do
+  -- print dpGroups -- FIXME: debug
   protocolsBrake <- initProtocolsBrake
   dpRequestors <- initDataPointRequestors
   connectedNodes <- initConnectedNodes
@@ -73,9 +83,10 @@ launchAcceptorsSimple mode localSock dpName = do
           }
 
   void . sequenceConcurrently $
-    [ runAcceptors tracerEnv
-    , runDataPointsPrinter dpName dpRequestors
-    ]
+      runAcceptors tracerEnv
+    : [ runDataPointsPrinter dpNames period dpRequestors 1
+      | (period,dpNames) <- dpGroups
+      ]
  where
   mkConfig = TracerConfig
     { networkMagic   = 764824073
@@ -93,21 +104,180 @@ launchAcceptorsSimple mode localSock dpName = do
     , metricsComp    = Nothing
     }
 
--- | To be able to ask any 'DataPoint' by the name without knowing the actual type,
---   we print it out as a raw 'ByteString'.
-runDataPointsPrinter
-  :: String
-  -> DataPointRequestors
+
+launchInitiators
+  :: Pico
+  -> [FilePath]
+  -> [(Seconds,[DPName])] -- for each period, a groups of datapoints to query
   -> IO ()
-runDataPointsPrinter dpName dpRequestors = forever $ do
-  sleep 1.0
-  dpReqs <- M.toList <$> readTVarIO dpRequestors
-  forM_ dpReqs $ \(_, dpReq) -> do
-    dpValues <- askForDataPoints dpReq [T.pack dpName]
-    forM_ dpValues $ \(dpName', dpValue) ->
-      case dpValue of
-        Nothing -> return ()
-        Just rawDPValue -> do
-          putStr $ "DataPoint, name: " <> T.unpack dpName' <> ", raw value: "
-          LBS.putStr rawDPValue
-          putStrLn ""
+launchInitiators ekgFreq localSocks dpGroups = do
+  protocolsBrake <- initProtocolsBrake
+  dpRequestors <- initDataPointRequestors
+  connectedNodes <- initConnectedNodes
+  acceptedMetrics <- initAcceptedMetrics
+  savedTO <- initSavedTraceObjects
+  currentLogLock <- newLock
+  void . sequenceConcurrently $
+      runAcceptors mkConfig connectedNodes acceptedMetrics savedTO
+                   dpRequestors protocolsBrake currentLogLock
+    : [ runDataPointsPrinter dpNames period dpRequestors (length localSocks)
+      | (period,dpNames) <- dpGroups
+      ]
+ where
+  mkConfig = TracerConfig
+    { networkMagic   = 764824073
+    , network        = ConnectTo $ NE.fromList $ map LocalSocket localSocks
+      -- NOTE: Nothing in the following two implies "default" values
+    , loRequestNum   = Just 1
+                       -- just one at a time, and what happens if we
+                       -- don't request enough?
+    , ekgRequestFreq = Just ekgFreq
+                     -- N.B.: this is the period (secs), not frequency (/sec)
+    , hasEKG         = Nothing
+    , hasPrometheus  = Nothing
+    , hasRTView      = Nothing
+    , logging        = NE.fromList [LoggingParams "/tmp/demo-acceptor" FileMode ForHuman]
+    , rotation       = Nothing
+    , verbosity      = Just Minimum
+      -- TODO: So, we can log minimum 'here', even if the "trace server"
+      -- is logging at a different verbosity?
+    }
+
+    -- TODO: figure out how to turn off logging.
+    
+---- new Datapoint abstractions ------------------------------------
+
+type DPName = String
+              -- hmmm,
+              --   - String because we used getArgs
+              --   - but elsewhere: type DataPointName   = Text
+              
+type Period = Seconds
+
+data DPType = DPT_Digest Int Period
+            | DPT_Value (Maybe Period)  -- ^ may not have a known period.
+
+
+-- the output of getDPs:
+
+data OutVal = OV_List [Value]  -- for digests, other?
+            | OV_Value Value   -- std Datapoints
+            deriving (Eq,Ord,Read,Show)
+
+data DPOut = DPO { dp       :: DPName
+                 , recvTime :: UTCTime
+                 , out      :: OutVal
+                 }
+             deriving (Eq,Ord,Read,Show)
+
+dpTypes :: M.Map DataPointName DPType
+dpTypes = M.fromList
+            [ ("Static.DatapointList" , DPT_Value Nothing )
+              -- Deprecated: remove soon!
+            , ("NodeInfo"             , DPT_Value Nothing )
+            , ("NodeState"            , DPT_Value Nothing )   
+            , ("NodePeers"            , DPT_Value Nothing )   
+            , ("New.BlockFetch.Client", DPT_Digest 30 50 )
+            , ("New.ChainSync.Client" , DPT_Digest 30 50 )
+            ]
+
+  -- statically known for each version.
+
+---- handle datapoints ---------------------------------------------
+
+-- | To be able to ask any 'DataPoint' by the name without knowing the
+--   actual type, we print it out as a raw 'ByteString'.
+--
+-- However, each ByteString *should*
+--  1. be parseable as JSON
+--  2. succeed for parseJSON (in FromJSON class) for the type
+--     that was originally encoded into the datapoint.
+
+runDataPointsPrinter
+  :: [DPName]
+  -> Seconds
+  -> DataPointRequestors
+  -> Int
+  -> IO ()
+runDataPointsPrinter dpNames wait dpRequestors lenRequestors =
+  do
+  digestLastTimeMaps <- do
+    let zeroDay = UTCTime (ModifiedJulianDay 0) 0 
+               -- lowest value, in case clocks not syncd
+        digestDpMap = M.filter
+                        (\case {DPT_Digest{} -> True; _ -> False})
+                        dpTypes
+    sequence $ replicate lenRequestors
+             $ mapM (\_->newIORef zeroDay) digestDpMap
+
+  forever $ do
+    dpReqs <- M.toList <$> readTVarIO dpRequestors
+      -- length should expect to be either 0 or numRequestors!
+    putStrLn $ ":DEBUG: dpReqs: " ++ show(length dpReqs)
+    hFlush stdout 
+    forM_ (zip dpReqs [0..]) $ \((nid, dpReq),reqIndex) -> do
+      dpValues <- askForDataPoints dpReq (map T.pack dpNames)
+      forM_ dpValues $ \(dpName, mValue) ->
+        case mValue of
+          Nothing -> do
+            putStrLn $ ":WARN: no datapoint returned ("
+                       <> T.unpack dpName <> ")"
+            hFlush stdout
+          Just value -> do
+            case M.lookup dpName dpTypes of
+              Just (DPT_Digest m p) -> do
+                      putStr $ ":INFO: DataPoint Digest:"
+                            <> " node: " <> show nid
+                            <> " name: " <> T.unpack dpName <> " "
+                      handleDP_Digest
+                        (digestLastTimeMaps !! reqIndex M.! dpName) m p value
+              Just (DPT_Value  mp ) -> handleDP_Value  mp  dpName value
+              Nothing               -> putStrLn $ ":ERROR: unknown datapoint"
+
+    sleep wait
+      -- FIXME[F3]: the "period" is not exactly 'wait'
+  
+    -- FIXME[F3]: The first time through:
+    --   - this will sleep for 'wait' secs
+    --   - as dpRequestors contains Map.empty in this case.
+
+  
+handleDP_Digest ::
+  IORef UTCTime -> Int -> Period -> DataPointValue -> IO ()
+handleDP_Digest rLasttime _ _ v = do
+  case decode v :: Maybe [(UTCTime,Value)] of
+    Nothing   -> putStrLn "ERROR: <CANNOT PARSE>"
+    Just logs -> do
+                 lasttime <- readIORef rLasttime
+                 let logs' = takeWhile (\(t,_)-> t > lasttime) logs
+                     llogs' = length logs'
+                 putStrLn $ show llogs'
+                            <> " messages after time "
+                            <> show lasttime  -- FIXME
+                            <> ":"
+                 putStrLn $ ":INFO: from "
+                            <> show (length logs)
+                            <> " digest messages"
+                 hFlush stdout
+                 unless (null logs') $
+                   do
+                   forM_ (reverse logs') $
+                     \(t,v')-> do
+                               LBS.putStr (encode (t,v'))
+                               putChar '\n'
+                               hFlush stdout
+                   writeIORef rLasttime (fst $ head logs')
+                 -- FIXME[F3]: TODO:
+                 --  We have a lot of information to determine if
+                 --  we have lost any events ...
+                 
+  putChar '\n'
+  hFlush stdout
+
+handleDP_Value  :: Maybe Period -> DataPointName -> DataPointValue -> IO ()
+handleDP_Value _ dpName v = do
+  putStr $ "DataPoint, name: " <> T.unpack dpName <> ", raw value: "
+  LBS.putStr v
+  putChar '\n'
+  hFlush stdout
+  
